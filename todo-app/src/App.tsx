9f73bbb572d3791d75d50d065b5e05e56210fa43
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-08 12:12:05 +0530

    add context api

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import TodoDetails from "./components/TodoDetails";
 import Header from "./components/Header";
 import TodoApp from "./components/TodoApp";
@@ -8,20 +7,24 @@ import Search from "./components/Search";
 import SortTodos from "./components/SortTodos";
 import ShowCompletedData from "./components/ShowCompletedData";
 
+import { DataProvider } from "./context/RouterContext";
+
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<TodoApp />} />
-          <Route path="/show-all" element={<ShowAllTodo />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/sort" element={<SortTodos />} />
-          <Route path="/filter" element={<ShowCompletedData />} />
-          <Route path="/todo-details/:id" element={<TodoDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <DataProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<TodoApp />} />
+            <Route path="/show-all" element={<ShowAllTodo />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/sort" element={<SortTodos />} />
+            <Route path="/filter" element={<ShowCompletedData />} />
+            <Route path="/todo-details/:id" element={<TodoDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </DataProvider>
     </>
   );
 };
