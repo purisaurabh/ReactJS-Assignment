4833ab6fcf207b1aa86f0b54e1e903f01ec392e7
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-12 14:59:12 +0530

    add reactjs-assignment 8 solution

--- todo-app/src/components/ShowAllTodo.tsx
+++ todo-app/src/components/ShowAllTodo.tsx
@@ -8,41 +8,89 @@ import useDelete from "../customHooks/useDelete";
 import useUpdate from "../customHooks/useUpdate";
 
 const initialValue = {
-  id: "",
-  title: "",
-  completed: false,
+  todos: [],
+  loading: true,
+  error: null,
 };
 
 const reducerAction = {
-  CALL_API: "call-api",
+  setTodo: "setTodo",
+  setLoading: "setLoading",
+  setError: "setError",
 };
 
-const reducer = (state: TodoItem, action: any) => {
+const payloadTypes = {};
+
+const actions = {
+  type: reducerAction,
+  payload: payloadTypes,
+};
+
+interface TodoAction {
+  type: "setTodo";
+  payload: TodoItem[];
+}
+interface LoadingAction {
+  type: "setLoading";
+  payload: boolean;
+}
+
+interface ApiDataAction {
+  type: "apiData";
+  payload: TodoItem[];
+}
+
+function setTodo(data: TodoItem[]): TodoAction {
+  return {
+    type: "setTodo",
+    payload: data,
+  };
+}
+
+function apiData(data: TodoItem[]): ApiDataAction {
+  return {
+    type: "apiData",
+    payload: data,
+  };
+}
+
+type Action = TodoAction | LoadingAction | ApiDataAction;
+
+const reducer = (state: any, action: Action) => {
   switch (action.type) {
-    case reducerAction.CALL_API:
-      return { ...state };
+    case "apiData":
+      ///
+      return { ...state, todos: action.payload, loading: false };
+    case "setTodo":
+      return { ...state, todos: action.payload };
+    case "setLoading":
+      return { ...state, loading: action.payload };
   }
 };
 
 const ShowAllTodo = () => {
   const data = useContext(DataContext);
-  const [todos, setTodos] = useState<TodoItem[]>([]);
-  const [loading, setLoading] = useState(true);
+  // const [todos, setTodos] = useState<TodoItem[]>([]);
+  // const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string>("");
   const [showCompleted, setShowCompleted] = useState(false);
   const { deleteData } = useDelete();
   const { updateData } = useUpdate();
 
   const [state, dispatch] = useReducer(reducer, initialValue);
+  const { todos, loading } = state;
 
   useEffect(() => {
-    dispatch({ type: reducerAction.CALL_API });
-    setTodos(data);
-    setLoading(false);
+    dispatch(apiData(data));
+    // dispatch({ type: "setTodo", payload: data });
+    // dispatch({ type: "setLoading", payload: false });
+
+    // setTodos(data);
+    // setLoading(false);
   }, [data]);
 
   const displayTodos = showCompleted
-    ? todos.filter((todo) => todo.completed === true)
+    ? todos.filter((todo: any) => todo.completed === true)
     : todos;
 
   const deleteTodo = async (id: string) => {
@@ -50,7 +98,11 @@ const ShowAllTodo = () => {
     if (flagValue) {
       alert("Delete Successfully");
     }
-    setTodos(todos.filter((todo) => todo.id !== id));
+    // setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "setTodo",
+      payload: todos.filter((todo: any) => todo.completed === true),
+    });
   };
 
   const markedTodoCompleted = async (id: string, completed: boolean) => {
@@ -59,16 +111,27 @@ const ShowAllTodo = () => {
       if (flagValue) {
         alert("Mark as completed");
       }
-      setTodos(
-        todos.map((todo) => {
+      // setTodos(
+      //   todos.map((todo: any) => {
+      //     if (id === todo.id) {
+      //       updateData(id, completed);
+      //       return { ...todo, completed: completed };
+      //     } else {
+      //       return todo;
+      //     }
+      //   })
+      // );
+      dispatch({
+        type: "setTodo",
+        payload: todos.map((todo: any) => {
           if (id === todo.id) {
             updateData(id, completed);
             return { ...todo, completed: completed };
           } else {
             return todo;
           }
-        })
-      );
+        }),
+      });
     } catch (error) {
       console.log("Error occured");
     }
