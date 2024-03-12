049de0f938e56c332475857df094946be8e0005c
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-12 15:16:27 +0530

    delete unneccessary code

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import TodoApp from './components/TodoApp';
-import TodoDetails from './components/TodoDetails';
-
+import TodoApp from "./components/TodoApp";
+import TodoDetails from "./components/TodoDetails";
 
 const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<TodoApp />} />
-          {/* <Route path='/add-todo' element={<Task />}/>
-              <Route path='/all' element={<ShowAllTask />} /> */}
-          <Route path='/todo-details/:id' element={<TodoDetails />} />
+          <Route path="/" element={<TodoApp />} />
+          <Route path="/todo-details/:id" element={<TodoDetails />} />
         </Routes>
       </BrowserRouter>
-
-
     </>
   );
-}
+};
 
 export default App;
