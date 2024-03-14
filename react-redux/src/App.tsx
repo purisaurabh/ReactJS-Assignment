4accc858d5af532f4d37ffb97370e18938fceabd
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-14 22:38:30 +0530

    update assignment 9 solution

--- react-redux/src/App.tsx
+++ react-redux/src/App.tsx
@@ -1,16 +1,15 @@
 // App.tsx
 import React from "react";
 import { Provider } from "react-redux";
-import Counter from "./counter"; // Assuming the Counter component is in a file named Counter.tsx
-import { Store } from "./store"; // Import your Redux store
+import { Counter } from "./components/Counter";
+import store from "./api/store";
 
 const App: React.FC = () => {
   return (
-    <Provider store={Store}>
+    <Provider store={store}>
       {" "}
       <div className="App">
-        <h1>React Redux</h1>
-        <Counter /> {/* Render the Counter component */}
+        <Counter />
       </div>
     </Provider>
   );
