9adce28d3475838d130eb2737824d5303b2694e9
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-14 12:38:45 +0530

    add useReducer in Sort Function

--- todo-app/src/components/SortTodos.tsx
+++ todo-app/src/components/SortTodos.tsx
@@ -1,23 +1,63 @@
-import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import React, {
+  ChangeEvent,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import useFetch from "../customHooks/useFetch";
 import { TodoItem } from "../utils/interface";
 import ShowSearchTodo from "./ShowSearchTodo";
 import { DataContext } from "../context/RouterContext";
 
+interface StateType {
+  todos: TodoItem[];
+  sortData: TodoItem[];
+  sortText: string;
+}
+
+const initialValue: StateType = {
+  todos: [],
+  sortData: [],
+  sortText: "",
+};
+
+// this is for the actions
+type Action =
+  | { type: "SET_TODOS"; payload: TodoItem[] }
+  | { type: "SET_SORT_DATA"; payload: TodoItem[] }
+  | { type: "SET_SORT_TEXT"; payload: string };
+
+const reducer = (state: StateType, action: Action) => {
+  switch (action.type) {
+    case "SET_TODOS":
+      return { ...state, todos: action.payload };
+    case "SET_SORT_DATA":
+      return { ...state, sortData: action.payload };
+    case "SET_SORT_TEXT":
+      return { ...state, sortText: action.payload };
+    default:
+      return state;
+  }
+};
+
 const SortTodos = () => {
-  const todoData = useContext(DataContext);
-  const [todos, setTodos] = useState<TodoItem[]>([]);
-  const [sortData, setSortData] = useState<TodoItem[]>([]);
-  const [sortText, setSortText] = useState("");
+  const todoData = useFetch();
+  // const [todos, setTodos] = useState<TodoItem[]>([]);
+  // const [sortData, setSortData] = useState<TodoItem[]>([]);
+  // const [sortText, setSortText] = useState("");
+
+  const [state, dispatch] = useReducer(reducer, initialValue);
 
   useEffect(() => {
-    setTodos(todoData);
+    // setTodos(todoData);
+    dispatch({ type: "SET_TODOS", payload: todoData });
     console.log({ todoData });
   }, [todoData]);
 
   const sortByName = (order: string) => {
     // sort are modify the original array have to create the copt
-    const todoCopy = [...todos];
+    const todoCopy = [...state.todos];
     const sortedData = todoCopy.sort((a, b) => {
       const firstName = a.title.toLowerCase();
       const secondName = b.title.toLowerCase();
@@ -30,27 +70,29 @@ const SortTodos = () => {
       return 0;
     });
 
-    setSortData(sortedData);
+    // setSortData(sortedData);
+    dispatch({ type: "SET_SORT_DATA", payload: sortedData });
   };
 
   return (
     <>
       <input
         type="text"
-        value={sortText}
+        value={state.sortText}
         placeholder="Enter 'asc' or 'desc'"
         onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setSortText(e.target.value)
+          // setSortText(e.target.value)
+          dispatch({ type: "SET_SORT_TEXT", payload: e.target.value })
         }
       />
 
-      <button onClick={() => sortByName(sortText)}>Sort</button>
+      <button onClick={() => sortByName(state.sortText)}>Sort</button>
 
-      {sortText && sortData.length === 0 ? (
+      {state.sortText && state.sortData.length === 0 ? (
         <p>No data available</p>
       ) : (
         <ul>
-          {sortData.map((todo) => (
+          {state.sortData.map((todo) => (
             <li key={todo.id}>
               <ShowSearchTodo
                 id={todo.id}
