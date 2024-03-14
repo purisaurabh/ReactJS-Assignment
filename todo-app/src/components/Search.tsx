a6a814b4fda73e6da83edbc227d797fd3b53d3e7
Saurabh Puri <purisaurabh2885@gmail.com>
2024-03-14 12:37:39 +0530

    add useReducer in Search component

--- todo-app/src/components/Search.tsx
+++ todo-app/src/components/Search.tsx
@@ -1,53 +1,92 @@
-import React, { ChangeEvent, useContext, useEffect, useReducer, useState } from "react";
+import React, {
+  ChangeEvent,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { TodoItem } from "../utils/interface";
 import ShowSearchTodo from "./ShowSearchTodo";
 import { DataContext } from "../context/RouterContext";
+import useFetch from "../customHooks/useFetch";
 
+// this is for the state
+interface StateType {
+  todos: TodoItem[];
+  filterTodo: TodoItem[];
+  searchText: string;
+}
 
+const initialValue: StateType = {
+  todos: [],
+  filterTodo: [],
+  searchText: "",
+};
+
+// this is for the actions
+type Action =
+  | { type: "SET_TODOS"; payload: TodoItem[] }
+  | { type: "SET_FILTER_DATA"; payload: TodoItem[] }
+  | { type: "SET_SEARCH_TEXT"; payload: string };
+
+const reducer = (state: StateType, action: Action) => {
+  switch (action.type) {
+    case "SET_TODOS":
+      return { ...state, todos: action.payload };
+    case "SET_FILTER_DATA":
+      return { ...state, filterTodo: action.payload };
+    case "SET_SEARCH_TEXT":
+      return { ...state, searchText: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Search = () => {
-  const todoData = useContext(DataContext);
-  const [todos, setTodos] = useState<TodoItem[]>([]);
-  const [fileredData, setFilteredData] = useState<TodoItem[]>([]);
-  const [searchText, setSearchText] = useState("");
+  const todoData = useFetch();
+  // const [todos, setTodos] = useState<TodoItem[]>([]);
+  // const [fileredData, setFilteredData] = useState<TodoItem[]>([]);
+  // const [searchText, setSearchText] = useState("");
 
-  
+  const [state, dispatch] = useReducer(reducer, initialValue);
 
   useEffect(() => {
-    setTodos(todoData);
-    console.log({ todoData });
+    dispatch({ type: "SET_TODOS", payload: todoData });
+    // setTodos(todoData);
   }, [todoData]);
 
   return (
     <>
       <input
-        value={searchText}
+        value={state.searchText}
         placeholder="enter value to search.."
         onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setSearchText(e.target.value)
+          // setSearchText(e.target.value)
+          dispatch({ type: "SET_SEARCH_TEXT", payload: e.target.value })
         }
       />
 
       <button
         onClick={() => {
-          if (todos) {
-            const searchedText = todos.filter(
+          if (state.todos) {
+            const searchedText = state.todos.filter(
               (res) =>
                 res.title &&
-                res.title.toLowerCase().includes(searchText.toLowerCase())
+                res.title.toLowerCase().includes(state.searchText.toLowerCase())
             );
-            setFilteredData(searchedText);
+            // setFilteredData(searchedText);
+            dispatch({ type: "SET_FILTER_DATA", payload: searchedText });
           }
         }}
       >
         Search
       </button>
 
-      {searchText && fileredData.length === 0 ? (
+      {state.searchText && state.filterTodo.length === 0 ? (
         <p>No data available</p>
       ) : (
         <ul>
-          {fileredData.map((todo) => (
+          {state.filterTodo.map((todo) => (
             <li key={todo.id}>
               <ShowSearchTodo
                 id={todo.id.toString()}
